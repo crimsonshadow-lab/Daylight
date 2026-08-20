a7ae1484ec176dd897316bac0a075601c98c44a4
crimsonshadow-lab <71266127+crimsonshadow-lab@users.noreply.github.com>
2026-08-20 17:29:34 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,18 @@
-const CACHE = "daylight-v2";
+// All paths are relative to this file, so this works at both / and /Daylight/.
+const CACHE = "daylight-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./src/app.js", "./src/planner.js", "./src/parser.js", "./src/store.js", "./manifest.webmanifest", "./icons/icon-192.svg", "./icons/icon-512.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy=response.clone();
+    if(response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(cache => cache.put(event.request,copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || (event.request.mode === "navigate" ? caches.match("./index.html") : Response.error()))));
+});
